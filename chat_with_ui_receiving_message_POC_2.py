11bda7f4847157f6805a46fcdb3718094ee617cc
Prometeus <evgeniy.jeka@gmail.com>
2022-10-17 14:21:06 +0300

    UI building Button added.

--- chat_with_ui_receiving_message_POC_2.py
+++ chat_with_ui_receiving_message_POC_2.py
@@ -54,6 +54,7 @@ class ChatRoom:
             @sio.on('received_message')
             def handle_my_custom_event(message):
 
+                # Remove this validation? All messages shall be eventually printed to the TEXT Messages Box
                 if my_name != message['sender']:
                     print(f"{message['sender']}: {message['content']}")
                     first_message_conversation = f"{message['sender']}: {message['content']}"
@@ -72,7 +73,9 @@ class ChatRoom:
                         current_messages_box.insert(INSERT, f"{message['sender']}: {message['content']}")
 
             message = input()
-            sio.emit('client_sends_message', {'sender':my_name, "content":message, "conversation_room": conversation_room})
+
+            # Move to a separate method and link to the 'SEND' button.
+            sio.emit('client_sends_message', {'sender': my_name, "content": message, "conversation_room": conversation_room})
 
 
     def show_message_box(self, first_mesage, message_sender):
@@ -81,22 +84,21 @@ class ChatRoom:
         hight = 500
         width = 900
         size = '%sx%s' % (width, hight)
-        columns_size = 50
 
         # Window
-        root = Tk()
-        root.geometry(size)
-        root.resizable(0, 0)
+        message_box_window = Tk()
+        message_box_window.geometry(size)
+        message_box_window.resizable(0, 0)
 
-        print("Inside show-message-box")
+        #print("Inside show-message-box")
 
-        # Text
-        messages_box = Text(root, height=20, width=105)
+        # Messages Box - TK 'Text' object
+        messages_box = Text(message_box_window, height=20, width=105)
         messages_box.grid(row=4, column=0, padx=25, pady=10)
         messages_box.grid(columnspan=10)
 
         # Entry box
-        created_entry = Entry(root, width="140")
+        created_entry = Entry(message_box_window, width="140")
         created_entry.grid(row=7, column=1, padx=25, pady=5)
 
         messages_box.insert(INSERT, first_mesage)
@@ -104,55 +106,60 @@ class ChatRoom:
 
         self.address_book[message_sender] = messages_box
 
+        # SEND button - the text from the entry box will be packed to a WS message and the former will be
+        # emitted to the conversation room. (Remove 'not my message' validation?)
+        button_send = Button(message_box_window, text="Send", bg="purple", fg="white", height="2", width="10")
+        button_send.grid(row=14, column=0)
 
-        root.mainloop()
+        message_box_window.mainloop()
 
-        # secondary = Tk()
-        # secondary.geometry("550x100")
+        # If window is closed (by clicking on X) - set self.address_book[message_sender] = None
 
-        # label_1 = Label(secondary, text=f"Entry {self.cnt}:", fg="blue", font=("", 11))
-        # created_entry = Entry(secondary, width="70")
-        # created_entry.num = self.cnt
-        #
-        # label_1.grid(row=0, column=0, sticky=E)
-        # created_entry.grid(row=0, column=1)
-        #
-        # created_entry.insert(0, first_mesage)
-        #
-        # self.address_book[message_sender] = created_entry
-        #
-        # self.cnt += 1
-        #secondary.mainloop()
 
 
 if __name__ == '__main__':
-    chtr = ChatRoom()
+    #chtr = ChatRoom()
+
+    # Window size
+    hight = 500
+    width = 900
+    size = '%sx%s' % (width, hight)
+
+    # Window
+    message_box_window = Tk()
+    message_box_window.geometry(size)
+    message_box_window.resizable(0, 0)
+
+    # print("Inside show-message-box")
+
+    # Messages Box - TK 'Text' object
+    messages_box = Text(message_box_window, height=20, width=105)
+    messages_box.grid(row=4, column=0, padx=25, pady=10)
+    messages_box.grid(columnspan=10)
+
+    # Entry box
+    created_entry = Entry(message_box_window, width="140")
+    created_entry.grid(row=7, column=3, padx=25, pady=5, sticky=W)
+
+    messages_box.insert(INSERT, 'loop')
+    messages_box.insert(INSERT, "\n")
+
+    #self.address_book[message_sender] = messages_box
 
 
+    # SEND button - the text from the entry box will be packed to a WS message and the former will be
+    # emitted to the conversation room. (Remove 'not my message' validation?)
+    button_send = Button(message_box_window, text="Send", bg="purple", fg="white", height="2", width="30")
+    button_send.place(x=25, y=380)
 
-    # # Window size
-    # hight = 500
-    # width = 900
-    # size = '%sx%s' % (width, hight)
-    # columns_size = 50
-    #
-    # # Window
-    # root = Tk()
-    # root.geometry(size)
-    # root.resizable(0, 0)
-    #
-    # # Text
-    # messages_box = Text(root, height=20, width=105)
-    # messages_box.grid(row=4, column=0, padx=25, pady=10)
-    # messages_box.grid(columnspan=10)
-    #
-    # # Entry box
-    # created_entry = Entry(root, width="140")
-    # created_entry.grid(row=7, column=1, padx=25, pady=5)
-    #
-    # messages_box.insert(INSERT, "Hello.")
-    # messages_box.insert(INSERT, "\n")
-    # messages_box.insert(INSERT, "Test")
-    #
-    #
-    # root.mainloop()
+
+    def on_closing():
+        print("Handling WINDOW CLOSED - add //self.address_book[message_sender] = None/")
+        message_box_window.destroy()
+
+    message_box_window.protocol("WM_DELETE_WINDOW", on_closing)
+    message_box_window.mainloop()
+
+    message_box_window.mainloop()
+
+    # If window is closed (by clicking on X) - set self.address_book[message_sender] = None
