8d1968df8c6d553ab7d5958b783184d2f4606512
Prometeus <evgeniy.jeka@gmail.com>
2023-02-19 19:03:09 +0200

    JWT to Redis - fetching data from config file / env variables.

--- server_side/redis_integration.py
+++ server_side/redis_integration.py
@@ -1,25 +1,56 @@
+import configparser
 import redis
 import logging
+import os
 
 REDIS_PORT = 6379
-REDIS_HOST = 'localhost'
-REDIS_TOKENS_HASH_MAP_NAME = 'auth_tokens'
+
 
 class RedisIntegration:
-
-    # Copy this to Redis, fetch on app start
-    active_tokens = dict()
 
     redis_client = None
 
     def __init__(self, config_file_path):
-        self.redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
+        try:
+            self.read_config(config_file_path)
+            self.redis_client = redis.Redis(host=self.hst, port=REDIS_PORT, db=self.db_number)
+
+        except Exception as e:
+            logging.critical(f"Redis integration: Error! Failed to connect to Redis DB container - {e}")
+            raise e
+
+    def read_config(self, config_file_path):
+        """
+            This method reads database configuration information from either the provided configuration file
+            or environment variables.
+
+            :param config_file_path: (str) Path to the configuration file.
+            :raises: Exception if the database configuration information cannot be read.
+        """
+
+        try:
+            if os.getenv("SQL_USER") is None:
+                # Reading DB name, host and credentials from config
+                config = configparser.ConfigParser()
+                config.read(config_file_path)
+                self.hst = config.get("REDIS", "host")
+                self.redis_jwt_hashmap_name = config.get("REDIS", "auth_token_hashmap")
+                self.db_number = int(config.get("REDIS", "db_number"))
+            else:
+                self.hst = os.getenv("REDIS_HOST")
+                self.redis_jwt_hashmap_name = os.getenv("AUTH_TOKEN_HASHMAP")
+                self.db_name = int(os.getenv("REDIS_DB_NUMBER"))
+
+        except Exception as e:
+            logging.critical(f"Redis integration: Error! Failed to read config from the "
+                             f"'config.ini' file or fetch from environment variables! {e}")
+            raise e
 
     def insert_token(self, username, inserted_token):
 
         try:
             logging.info(f"Inserting  the JWT to Redis, username: {username}, token: {inserted_token}")
-            return self.redis_client.hset(REDIS_TOKENS_HASH_MAP_NAME, username, inserted_token)
+            return self.redis_client.hset(self.redis_jwt_hashmap_name, username, inserted_token)
 
         except Exception as e:
             logging.error(f"Redis Integration: Error! Failed to insert user token {username} into Redis - {e}")
@@ -28,7 +59,7 @@ class RedisIntegration:
     def fetch_token_by_username(self, username):
         try:
             logging.info(f"Fetching JWT from Redis by username: {username}")
-            user_token = self.redis_client.hget(REDIS_TOKENS_HASH_MAP_NAME, username)
+            user_token = self.redis_client.hget(self.redis_jwt_hashmap_name, username)
 
             if user_token:
                 return user_token.decode('utf-8')
@@ -58,7 +89,7 @@ class RedisIntegration:
     def delete_token(self, username):
         try:
             logging.info(f"Deleting the token  related to {username}from Redis")
-            self.redis_client.hdel(REDIS_TOKENS_HASH_MAP_NAME, username)
+            self.redis_client.hdel(self.redis_jwt_hashmap_name, username)
             return True
 
         except Exception as e:
@@ -66,26 +97,8 @@ class RedisIntegration:
             raise e
 
 
+#
+# if __name__ == "__main__":
+#     print("Test")
 
-if __name__ == "__main__":
-    print("Test")
-    config_file_path = "./config.ini"
-    me_redis = RedisIntegration(config_file_path)
-    me_redis.delete_token("Mioau")
-    #me_redis.insert_token("Mioau", "Miau_token")
-    print(me_redis.fetch_token_by_username("Mioau"))
-
-
-
-
-    # # Connect to Redis
-    # r = redis.Redis(host='localhost', port=6379, db=0)
-    #
-    # # Create a hash and add a record to it
-    # r.hset('myhash', 'key1', 'value1')
-    # r.hset('myhash', 'key2', 'test_me_value')
-    #
-    # # Retrieve the record from the hash
-    # value = r.hget('myhash', 'key2')
-    # print(value.decode('utf-8'))
 
