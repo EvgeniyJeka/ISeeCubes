dbf69028cbacc8d3c9536a8db04b771412df305c
Prometeus <evgeniy.jeka@gmail.com>
2023-02-21 19:08:02 +0200

    Server and Client - minor code improvements

--- clients/lisa/local_client_config.py
+++ clients/lisa/local_client_config.py
@@ -9,3 +9,7 @@ class LoginWindowErrorMessages(Enum):
     WRONG_CREDENTIALS = "Wrong credentials. Please re login with your credentials."
     SERVER_UNAVAILABLE = "Log In failed. Chat server isn't available at the moment."
     UNKNOWN_SERVER_CODE = "Log In failed. Server side error."
+
+
+class MainWindowConfig(Enum):
+    MAIN_UI_WINDOW_SIZE = "600x285"
